c05a4ed568d4d74ae3611a53a95a8d6a7fa5bcaa
Rama9121 <gandetisitaram@gmail.com>
2023-12-11 21:35:41 +0530

    my first code

--- text.py
+++ text.py
@@ -3,57 +3,54 @@ from PIL import Image,ImageTk
 from random import randint
 root=Tk()
 root.title('rock scissor paper')
-root.configuration(background='#9b59b6')
+root.configure(background='#9b59b6')
 #picture
-rock_img=ImageTk.photoImage(Image.open('rock-user.png'))
-paper_img=ImageTk.photoImage(Image.open('paper-user.png'))
-scissor_img=ImageTk.photoImage(Image.open('scissor-user.png'))
-rock_img_comp=ImageTk.photoImage(Image.open('rock.png'))
-paper_img_comp=ImageTk.photoImage(Image.open('paper.png'))
-scissor_img_comp=ImageTk.photoImage(Image.open('scissor.png'))
+rock_img=ImageTk.PhotoImage(Image.open('rock-user.png'))
+paper_img=ImageTk.PhotoImage(Image.open('paper-user.png'))
+scissor_img=ImageTk.PhotoImage(Image.open('scissor-user.png'))
+rock_img_comp=ImageTk.PhotoImage(Image.open('rock.png'))
+paper_img_comp=ImageTk.PhotoImage(Image.open('paper.png'))
+scissor_img_comp=ImageTk.PhotoImage(Image.open('scissor.png'))
 #insert picture
-user_label=Label(root,Image=scissor_img,bg='#9b59b6')
-comp_label=Label(root,Image=scissor_img,bf='#9b59b6')
+user_label=Label(root, image=scissor_img, bg="#9b59b6")
+comp_label=Label(root, image=scissor_img_comp, bg='#9b59b6')
 comp_label.grid(row=1,column=0)
 user_label.grid(row=1,column=4)
 #score
-playerscore=Label(root,text=0,font=100,bg='',fg='')
-computerscore=Label(root,text=0,font=100,bg='',fg='')
+playerscore=Label(root,text=0,font=100,bg='#9b59b6',fg='white')
+computerscore=Label(root,text=0,font=100,bg='#9b59b6',fg='white')
 computerscore.grid(row=1,column=1)
 playerscore.grid(row=1,column=3)
 #buttons
-rock=Button(root,width=20,height=2,text='rock',bg='',fg='white').grid(row=2,column=1)
-paper=Button(root,width=20,height=2,text='paper',bg='',fg='white').grid(row=2,column=2)
-scissor=Button(root,width=20,height=2,text='scissor',bg='',fg='white').grid(row=2,column=3)
+rock=Button(root,width=20,height=2,text='rock',bg='#FF3E4D',fg='white',command=lambda:updatechoice('rock')).grid(row=2,column=1)
+paper=Button(root,width=20,height=2,text='paper',bg='#FAD02E',fg='white',command=lambda:updatechoice('paper')).grid(row=2,column=2)
+scissor=Button(root,width=20,height=2,text='scissor',bg='#0ABDE3',fg='white',command=lambda:updatechoice('scissor')).grid(row=2,column=3)
 #indicator
-user_indicator=Label(root,font=50,text='user',bg='',fg='white')
-comp_indicator=Label(root,font=50,text='computer',bg='',fg='white')
+user_indicator=Label(root,font=50,text='user',bg='#9b59b6',fg='white')
+comp_indicator=Label(root,font=50,text='computer',bg='#9b59b6',fg='white')
 user_indicator.grid(row=0,column=3)
-comp_label.grid(row=0,column=1)
+comp_indicator.grid(row=0,column=1)
 #messages
-msg=Label(root,font=50,bg='',fg='white')
-msg.grid(row=3,column=3)
+msg=Label(root,font=50,bg='#9b59b6',fg='white')
+msg.grid(row=3,column=2)
 #update choice
 choices=['rock','paper','scissor']
 def updatechoice(x):
     if x=='rock':
-        user_label.configure(Image=rock_img)
+        user_label.configure(image=rock_img)
     elif x=='paper':
-        user_label.configure(Image=paper_img)
+        user_label.configure(image=paper_img)
     else:
-        user_label.configure(Image=scissor_img)
+        user_label.configure(image=scissor_img)
     checkwin(x,compchoice)
 #for computer
 compchoice=choices[randint(0,2)]
 if compchoice=='rock':
-    comp_label.configure(Image=rock_img_comp)
+    comp_label.configure(image=rock_img_comp)
 elif compchoice=='paper':
-    comp_label.configure(Image=paper_img_comp)
+    comp_label.configure(image=paper_img_comp)
 else:
-    comp_label.configure(Image=scissor_img_comp)
-
-#button
-COMMAND=lambda:updatechoice('rock')
+    comp_label.configure(image=scissor_img_comp)
 #update message
 def updatemessage(x):
     msg['text']=x
